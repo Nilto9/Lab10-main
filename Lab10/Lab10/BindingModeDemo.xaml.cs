f84256e4e66bfc2b592e872dbdc266be05bfc39c
Nilton <palian.nilton@hotmail.com>
2023-06-02 22:01:49 -0600

    first commit

---
+++ Lab10/Lab10/BindingModeDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lab10
+{	
+	public partial class BindingModeDemo : ContentPage
+	{	
+		public BindingModeDemo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ Lab10/Lab10/ViewToViewDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lab10
+{	
+	public partial class ViewToViewDemo : ContentPage
+	{	
+		public ViewToViewDemo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
